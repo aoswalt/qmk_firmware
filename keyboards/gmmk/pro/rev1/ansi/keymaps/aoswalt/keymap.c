3d76298650c9d1332f6adc91fca0c1faa941ddd1
Adam Oswalt <email@adamoswalt.com>
2026-03-20 14:43:22 -0500

    Fix numpad plus minus

--- keyboards/gmmk/pro/rev1/ansi/keymaps/aoswalt/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/aoswalt/keymap.c
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_CONTROL] = LAYOUT(
         _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          _______,
-        KC_ESC,  KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PPLS, KC_PMNS, KC_DEL,           KC_INS,
+        KC_ESC,  KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PMNS, KC_PPLS, KC_DEL,           KC_INS,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PAST, KC_PEQL, _______,          _______,
         _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          KC_PENT,          _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, KC_PCMM, KC_PDOT, KC_PSLS,          _______, _______, KC_HOME,
